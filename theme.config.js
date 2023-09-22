2c7078b93885f1e2a0c3a1a6489496570c563da2
boraxpr <tata-naipawat@hotmail.com>
2023-09-22 20:02:57 +0700

    Adjust CSS footer, article

--- theme.config.js
+++ theme.config.js
@@ -1,26 +1,17 @@
 import { SocialIcon } from 'react-social-icons';
-const YEAR = new Date().getFullYear()
+const YEAR = new Date().getFullYear();
+
 export default {
   darkMode: true,
   footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
-      <SocialIcon url="https://linkedin.com/in/naipawat-poolsawat" fgColor="#FFFFFF" style={{ float: 'right' }} />
-
-      <SocialIcon url="https://www.github.com/boraxpr" fgColor="#FFFFFF" style={{ float: 'right' }} />
-
-      <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
-      <time>{YEAR}</time> © Naipawat Poolsawat. All rights reserved.
-    </small>
-  )
-}
-
+    <footer>
+      <div>
+        <time>{YEAR}</time> © Naipawat Poolsawat. All rights reserved.
+      </div>
+      <div>
+        <SocialIcon url="https://linkedin.com/in/naipawat-poolsawat" fgColor="#FFFFFF" />
+        <SocialIcon url="https://www.github.com/boraxpr" fgColor="#FFFFFF" />
+      </div>
+    </footer>
+  ),
+};
